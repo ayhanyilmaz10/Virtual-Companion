1c2e200f2c54808f3099b05d072e66b5fed3eac9
Ayhan Yılmaz <ayhan_yilmaz10@outlook.com>
2025-12-24 21:17:33 +0300

    Virtual Companion2

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAghQE0SsLPHs-Cp5Muwo9XzJ1BdPOGtlE",
-  authDomain: "fir-e5dd9.firebaseapp.com",
-  projectId: "fir-e5dd9",
-  storageBucket: "fir-e5dd9.firebasestorage.app",
-  messagingSenderId: "871087435639",
-  appId: "1:871087435639:web:407c673ac994d645c2bb72",
-  measurementId: "G-3DB41RP4KZ"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
